78eae0b77ab352a973b264d6b11b027d936e31b9
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-07-06 12:11:20 -0300

    feat: create function control inputs

--- src/components/formCalculator/InputWalls.tsx
+++ src/components/formCalculator/InputWalls.tsx
@@ -1,12 +1,27 @@
+import { ChangeEventHandler } from 'react'
+
 type Props = {
   labelWall: string
+  heightWidth: string
+  valueInput: number
+  handleOnChange: ChangeEventHandler<HTMLInputElement>
 }
 
-function InputWalls({ labelWall }: Props) {
+function InputWalls({
+  labelWall,
+  heightWidth,
+  valueInput,
+  handleOnChange
+}: Props) {
   return (
     <label htmlFor="">
       {labelWall}
-      <input type="number" />
+      <input
+        value={valueInput}
+        name={heightWidth}
+        onChange={handleOnChange}
+        type="number"
+      />
     </label>
   )
 }
