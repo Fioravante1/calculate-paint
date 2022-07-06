c54cd2628f76fd0ff9dfbbf61c190a958bf69d83
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-07-06 12:08:11 -0300

    refactor: simple render input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,7 @@ import FormCalculator from '../components/formCalculator/FormCalculator'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <FormCalculator wallText="Parede 1:" />
-      <FormCalculator wallText="Parede 2:" />
-      <FormCalculator wallText="Parede 3:" />
-      <FormCalculator wallText="Parede 4:" />
+      <FormCalculator />
     </div>
   )
 }
