e1e8b3814317d1dc7c78aef0e09fc6242412288c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-07-06 12:08:46 -0300

    feat: create helper initiual commit

---
+++ src/helpers/initialStates.tsx
@@ -0,0 +1,32 @@
+export const heightWall = [
+  {
+    selectWindow: 'selectWindow 1',
+    selectDoor: 'selectDoor 1',
+    numberWall: 'Parede 1',
+    numberWallHeight: 'ParedeHeight1',
+    numberWallWidth: 'ParedeWidth1'
+  },
+  {
+    selectWindow: 'selectWindow 2',
+    selectDoor: 'selectDoor 2',
+    numberWall: 'Parede 2',
+    numberWallHeight: 'ParedeHeight2',
+    numberWallWidth: 'ParedeWidth2'
+  },
+  {
+    selectWindow: 'selectWindow 3',
+    selectDoor: 'selectDoor 3',
+    numberWall: 'Parede 3',
+    numberWallHeight: 'ParedeHeight3',
+    numberWallWidth: 'ParedeWidth3'
+  },
+  {
+    selectWindow: 'selectWindow 4',
+    selectDoor: 'selectDoor 4',
+    numberWall: 'Parede 4',
+    numberWallHeight: 'ParedeHeight4',
+    numberWallWidth: 'ParedeWidth4'
+  }
+]
+
+export const quantityWindonDoor = [0, 1, 2, 3, 4, 5]
